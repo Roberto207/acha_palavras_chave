53fe8603e7fcf65cda3eb4856d07ac73ab4b0feb
Roberto Caetano Neto <robertocaetano207@gmail.com>
2025-08-09 18:06:01 -0300

    Update and rename acha_palavras_chave.py to acha_palavras_chave3.py

--- acha_palavras_chave3.py
+++ acha_palavras_chave3.py
@@ -1,6 +1,3 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from collections import Counter
 import string
 import cohere
@@ -25,33 +22,7 @@ elif escolha == '2':
     roteiro = transcrever_audio(caminho_arquivo)
     print("\n--- Roteiro transcrito ---\n")
     print(roteiro)
-def extrair_palavras_chave(texto, n=5):
-    # Tokenização
-    palavras = word_tokenize(texto.lower())
-
-    # Remover pontuação e stopwords
-    stop_words = set(stopwords.words('portuguese') + list(string.punctuation))
-    palavras_filtradas = [p for p in palavras if p not in stop_words and len(p) > 2]
-
-    # Contar frequência
-    contagem = Counter(palavras_filtradas)
-    
-    # Retornar as n palavras mais comuns
-    return [palavra for palavra, _ in contagem.most_common(n)]
-
-palavras_chave = extrair_palavras_chave(roteiro)
-print("Palavras-chave extraídas:", palavras_chave)
-
-#outr modo de fazer 
-def achar_pchaves(roteieo):
-    # Estratégia simples: usar os 8 primeiros tokens relevantes
-    palavras = roteiro.split() #pegamo o texto trasncrito e fazemos uma lista com suas palavras
-    palavras_chave = [p for p in palavras if len(p) > 3][:8] #pegando as 8 palavras/tokens mais relevantes
-    consulta = ' '.join(palavras_chave) #ajustando a lista pra consulta
-    return consulta
-print(f'as palavras chave pelo metodo 2 foram :{achar_pchaves(roteiro)}')
-
-#outro modo de fazer
+    print("\n--------------------------\n")
 
 response = co.chat(
     model="command-a-03-2025",
@@ -63,4 +34,5 @@ response = co.chat(
         }
     ],
 )
-print('terceiro metodo:\n',response.message.content[0].text)
+
+print('terceiro metodo,palavras chave:\n',response.message.content[0].text)
